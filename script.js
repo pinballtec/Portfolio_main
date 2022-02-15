c702cf82bedad02f61f61096003f1dd806521251
pinballtec <daniilboiko99@gmail.com>
2022-02-15 22:34:22 +0100

    added JS functions(theme change)

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log('Its working')
+
+let Js_Dot = document.getElementsByClassName('Js-dot')
+
+for (var i=0; Js_Dot.length > i; i++){
+	Js_Dot[i].addEventListener('click' ,function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked: ', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if(mode == 'red'){
+		document.getElementById('theme-style').href = 'red.css'
+	}
+
+	if(mode == 'black'){
+		document.getElementById('theme-style').href = 'black.css'
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'blue.css'
+	}
+}
